22359b63b838b0d30793df65d6d174fb6f9f6d7f
tongyang <tongyang@diligrp.com>
2022-07-19 16:26:24 +0800

    feat: 环境配置+vite配置

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { wrapperEnv, createProxy } from './build/utils'
+import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+export default defineConfig(({command, mode}) => {
+  const env = loadEnv(mode, process.cwd())
+  const { VITE_PORT, VITE_PUBLIC_PATH, VITE_PROXY } = wrapperEnv(env);
+  
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      }
+    },
+    base: VITE_PUBLIC_PATH || '/',
+    css: {
+      preprocessorOptions: {
+        // define global less variable
+        less: {
+          additionalData: `@import '@/styles/variables.less';`
+        }
+      }
+    },
+    server: {
+      port: VITE_PORT,
+      proxy: createProxy(VITE_PROXY)
+    }
+  }
 })
