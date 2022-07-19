7840d6e87e05fbc4905bb53d7227a7ec2f1d60a2
tongyang <tongyang@diligrp.com>
2022-07-19 18:06:38 +0800

    feat: VueSetupExtend、visualizer、configHtmlPlugin配置

---
+++ build/plugin/index.js
@@ -0,0 +1,35 @@
+import vue from '@vitejs/plugin-vue'
+
+/**
+ * 扩展setup插件，支持在script标签中使用name属性
+ * usage: <script setup name="MyComp"></script>
+ * ? 在使用 Vue3.2 的 setup 语法糖后，无法优雅的定义组件的 name 值，
+ * ? 虽然 vite 会根据组件的文件名自动生成组件名，但是需要自定义的组件名时，就很不方便。
+ */
+import VueSetupExtend from 'vite-plugin-vue-setup-extend'
+
+// ? rollup进行打包分析
+import { visualizer } from 'rollup-plugin-visualizer'
+
+// ? 生成html配置
+import { configHtmlPlugin } from './html'
+
+export function createVitePlugins(viteEnv, isBuild) {
+    const plugins = [
+        vue(),
+        VueSetupExtend(),
+        configHtmlPlugin(viteEnv, isBuild)
+    ]
+
+    if (isBuild) {
+        plugins.push(
+            visualizer({
+                open: true,
+                gzipSize: true,
+                brotliSize: true
+            })
+        )
+    }
+
+    return plugins
+}
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig, loadEnv } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import { wrapperEnv, createProxy } from './build/utils'
+import { createVitePlugins } from './build/plugin'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({command, mode}) => {
-  const env = loadEnv(mode, process.cwd())
-  const { VITE_PORT, VITE_PUBLIC_PATH, VITE_PROXY } = wrapperEnv(env);
+  const env = loadEnv(mode, process.cwd());
+  const viteEnv = wrapperEnv(env);
+  const isBuild = command === 'build';
+  const { VITE_PORT, VITE_PUBLIC_PATH, VITE_PROXY } = viteEnv;
   
   return {
-    plugins: [vue()],
+    plugins: createVitePlugins(viteEnv, isBuild),
     resolve: {
       alias: {
         '@': path.resolve(__dirname, 'src'),
